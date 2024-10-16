088c8b00ec763d945c752e96fd9244429b5ef8b5
Teesta Naskar <63419282+TeestaNaskar@users.noreply.github.com>
2024-10-16 14:00:29 -0400

    Create DWLS_fromsignaturemat.sh

---
+++ DWLS_fromsignaturemat.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+#BSUB -P acc_DADisorders
+#BSUB -q premium
+#BSUB -n 1
+#BSUB -R span[ptile=4]
+#BSUB -R himem
+#BSUB -R rusage[mem=550000]
+#BSUB -W 144:00
+#BSUB -o dwlsstdout.%J.%I
+#BSUB -e dwlsstderr.%J.%I
+
+module load  R/4.0.3
+module load openssl
+module load udunits
+
+R CMD BATCH DWLS_fromsignaturematrix.R
